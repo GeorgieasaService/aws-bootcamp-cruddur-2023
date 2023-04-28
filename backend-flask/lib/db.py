18056df241cefa53234e65024c84a90f112a6222
GeorgeRBarnes <georgieintech@gmail.com>
2023-04-28 16:18:26 +0200

    activities troubleshooting

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -36,11 +36,11 @@ class Db:
     for key, value in params.items():
       print(key, ":", value)
       
-  def print_sql(self,title,sql,params={}):
+  def print_sql(self,title,sql):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql,params)
+    print(sql)
     
   def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql) 
@@ -60,7 +60,7 @@ class Db:
       
   def query_array_json(self,sql,params={}):
   # when we want to return an array of json objects
-    self.print_sql('array',sql,params) 
+    self.print_sql('array',sql) 
     
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
@@ -87,7 +87,7 @@ class Db:
           return "{}"
         else:
           return json[0]
-
+'''
   # when we want to return a a single value
   def query_value(self,sql,params={}):
     self.print_sql('value',sql,params)
@@ -96,6 +96,7 @@ class Db:
         cur.execute(sql,params)
         json = cur.fetchone()
         return json[0]
+'''
 
   def query_wrap_object(self,template):
     sql = f"""
