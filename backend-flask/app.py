0df241d30c81e2322d4b193981a53b422b5f30d7
George R. Barnes <georgieintech@gmail.com>
2023-04-28 12:34:40 +0000

    activities troubleshooting

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -253,11 +253,11 @@ def data_activities_reply(activity_uuid):
   else:
     return model['data'], 200
   return
-
+'''
 @app.route("/api/users/@<string:handle>/short", methods=['GET'])
 def data_users_short(handle):
   data = UsersShort.run(handle)
   return data, 200
-
+'''
 if __name__ == "__main__":
   app.run(debug=True)
