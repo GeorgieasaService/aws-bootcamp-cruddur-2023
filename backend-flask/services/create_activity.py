a558fab5cd2845cb5ea081932b1f691fa6e839b1
George R. Barnes <georgieintech@gmail.com>
2023-04-29 14:32:36 +0000

    troubleshooting to fix activities

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -58,7 +58,7 @@ class CreateActivity:
       expires_at = (now + ttl_offset)
       uuid =  CreateActivity.create_activity(user_handle, message, expires_at)
 
-      object_json = CreateActivity.query_object_activity()
+      object_json = CreateActivity.query_object_activity(uuid)
 
       model['data'] = object_json
     return model
