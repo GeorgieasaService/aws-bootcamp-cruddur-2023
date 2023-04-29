22c2c671e1443613344fdd544c9be9cf6ad80b46
George R. Barnes <georgieintech@gmail.com>
2023-04-29 14:27:54 +0000

    troubleshooting to fix activities

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -10,55 +10,55 @@ class CreateActivity:
       'expires_at': expires_at
     })
     
-def query_object_activity(uuid):
-  sql = db.template('activities','object')
-  return db.query_object_json(sql,{
-    'uuid': uuid
-    })
+  def query_object_activity(uuid):
+    sql = db.template('activities','object')
+    return db.query_object_json(sql,{
+      'uuid': uuid
+      })
 
-def run(user_handle, message, ttl):
-  model = {
-    'errors': None,
-    'data': None
-  }
+  def run(message, user_handle, ttl):
+    model = {
+      'errors': None,
+      'data': None
+    }
 
-  now = datetime.now(timezone.utc).astimezone()
+    now = datetime.now(timezone.utc).astimezone()
 
-  if (ttl == '30-days'):
-    ttl_offset = timedelta(days=30) 
-  elif (ttl == '7-days'):
-    ttl_offset = timedelta(days=7) 
-  elif (ttl == '3-days'):
-    ttl_offset = timedelta(days=3) 
-  elif (ttl == '1-day'):
-    ttl_offset = timedelta(days=1) 
-  elif (ttl == '12-hours'):
-    ttl_offset = timedelta(hours=12) 
-  elif (ttl == '3-hours'):
-    ttl_offset = timedelta(hours=3) 
-  elif (ttl == '1-hour'):
-    ttl_offset = timedelta(hours=1) 
-  else:
-    model['errors'] = ['ttl_blank']
+    if (ttl == '30-days'):
+      ttl_offset = timedelta(days=30) 
+    elif (ttl == '7-days'):
+      ttl_offset = timedelta(days=7) 
+    elif (ttl == '3-days'):
+      ttl_offset = timedelta(days=3) 
+    elif (ttl == '1-day'):
+      ttl_offset = timedelta(days=1) 
+    elif (ttl == '12-hours'):
+      ttl_offset = timedelta(hours=12) 
+    elif (ttl == '3-hours'):
+      ttl_offset = timedelta(hours=3) 
+    elif (ttl == '1-hour'):
+      ttl_offset = timedelta(hours=1) 
+    else:
+      model['errors'] = ['ttl_blank']
 
-  if user_handle == None or len(user_handle) < 1:
-    model['errors'] = ['user_handle_blank']
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['user_handle_blank']
 
-  if message == None or len(message) < 1:
-    model['errors'] = ['message_blank'] 
-  elif len(message) > 280:
-    model['errors'] = ['message_exceed_max_chars'] 
+    if message == None or len(message) < 1:
+      model['errors'] = ['message_blank'] 
+    elif len(message) > 280:
+      model['errors'] = ['message_exceed_max_chars'] 
 
-  if model['errors']:
-    model['data'] = {
-      'handle': user_handle,
-      'message': message
-    }   
-  else:
-    expires_at = (now + ttl_offset)
-    uuid =  CreateActivity.create_activity(user_handle, message, expires_at)
+    if model['errors']:
+      model['data'] = {
+        'handle': user_handle,
+        'message': message
+      }   
+    else:
+      expires_at = (now + ttl_offset)
+      uuid =  CreateActivity.create_activity(user_handle, message, expires_at)
 
-    object_json = CreateActivity.query_object_activity()
+      object_json = CreateActivity.query_object_activity()
 
-    model['data'] = object_json
-  return model
+      model['data'] = object_json
+    return model
