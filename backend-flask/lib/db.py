acd400a96413ce4faacb3005303393e0dbadb563
George R. Barnes <georgieintech@gmail.com>
2023-06-06 15:04:09 +0000

    debugging message groups not displaying

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -98,10 +98,7 @@ class Db:
       with conn.cursor() as cur:
         cur.execute(sql,params)
         json = cur.fetchone()
-        if json == None:
-          return None;
-        else:
-          return json[0]
+        return json[0]
         
   def query_wrap_object(self,template):
     sql = f"""
