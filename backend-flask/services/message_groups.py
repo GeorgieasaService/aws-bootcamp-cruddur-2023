9c9e2ceb291edb92507d11ea8a67bf6f4b405861
George R. Barnes <georgieintech@gmail.com>
2023-06-07 17:25:27 +0000

    message group json returning, debugging frontend issue displaying messages

--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -5,20 +5,17 @@ from lib.db import db # need the db to find the handle from the uuid
 
 class MessageGroups:
   def run(cognito_user_id):
-    print(f"printing the cognito_user_id: {cognito_user_id}")
     model = {
       'errors': None,
       'data': None
     }
-    
-    print('-----message_groups.py Testing-----')
 
+    sql = db.template('users','uuid_from_cognito_user_id')
     my_user_uuid = db.query_value(sql,{
       'cognito_user_id': cognito_user_id
     })
   
     print(f"UUID: {my_user_uuid}")
-    print('cognito_user_id in message_groups.py----', cognito_user_id)
 
     ddb = Ddb.client()
     data = Ddb.list_message_groups(ddb, my_user_uuid)
