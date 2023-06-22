9785a47b96e136b36d645f618a0f2d263e79563d
George R. Barnes <georgieintech@gmail.com>
2023-06-22 16:57:28 +0000

    finished setting up message groups

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,9 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown', 'cupola.potted-0k@icloud.com', '4f4baece-5966-4691-8ecd-f6d41f00b981'),
-  ('Andrew Bayko', 'bayko', 'sawbuck_bail_0m@icloud.com', 'b99a2447-162e-4dc7-a88c-bd8c669dbef4');
+  ('Andrew Bayko', 'bayko', 'sawbuck_bail_0m@icloud.com', 'b99a2447-162e-4dc7-a88c-bd8c669dbef4'),
+  ('Genie B', 'genie', 'genieb@bottles.com', 'mock');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
